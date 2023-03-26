ebb28b623f80bd0c8406a660689923f00fb1798a
iamtlcs <simoncheung2002@gmail.com>
2023-03-26 23:23:06 +0800

    Updated

--- .ipynb_checkpoints/AlexNet-checkpoint.py
+++ .ipynb_checkpoints/AlexNet-checkpoint.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-
+tf.config.run_functions_eagerly(True)
 #model=keras.models.Sequential([
 #    keras.layers.Conv2D(filters=128, kernel_size=(11,11), strides=(4,4), activation='relu', input_shape=(64,64,3)),
 #    keras.layers.BatchNormalization(),
@@ -24,21 +24,27 @@ from tensorflow import keras
 #])
 
 class AlexNet(keras.Model):
-	def __init__(self, input_shape = (227, 227, 3), activ_func_Conv2D = 'relu', activ_func_dense = 'relu', activ_func_output = 'softmax', droprate = 0.5num_classes = 8, **kwargs):
+    def __init__(self, input_shape = (227, 227, 3), activ_func_Conv2D = 'relu', activ_func_dense = 'relu', activ_func_output = 'softmax', droprate = 0.5, num_classes = 8, **kwargs):
         super(AlexNet, self).__init__(**kwargs)
         # Block 1
         self.input_Conv2D = keras.layers.Conv2D(filters=96, kernel_size=(11,11), strides=(4,4), activation=activ_func_Conv2D, input_shape=input_shape, padding="same", name="Block1_Conv")
+        self.batchnorm1 = keras.layers.BatchNormalization()
         self.MaxPool2D1 = keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2), name="Block1_pool")
         # Block 2
         self.Conv2D1 = keras.layers.Conv2D(filters=256, kernel_size=(5,5), strides=(1,1), activation=activ_func_Conv2D, padding="same", name="Block2_Conv")
-		self.MaxPool2D2 = keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2), name="Block2_pool")
+        self.batchnorm2 = keras.layers.BatchNormalization()
+        self.MaxPool2D2 = keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2), name="Block2_pool")
         # Block 3
         self.Conv2D2 = keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation=activ_func_Conv2D, padding="same", name="Block3_Conv1")
+        self.batchnorm3 = keras.layers.BatchNormalization()
         self.Conv2D3 = keras.layers.Conv2D(filters=384, kernel_size=(3,3), strides=(1,1), activation=activ_func_Conv2D, padding="same", name="Block3_Conv2")
+        self.batchnorm4 = keras.layers.BatchNormalization()
         self.Conv2D4 = keras.layers.Conv2D(filters=256, kernel_size=(3,3), strides=(1,1), activation=activ_func_Conv2D, padding="same", name="Block3_Conv3")
+        self.batchnorm5 = keras.layers.BatchNormalization()
         self.MaxPool2D3 = keras.layers.MaxPool2D(pool_size=(3,3), strides=(2,2), name="Block3_pool")
         # Classification block
-		self.fc1 = keras.layers.Dense(4096, activation=activ_func_dense, name="fc1")
+        self.flatten_layer = keras.layers.Flatten()
+        self.fc1 = keras.layers.Dense(4096, activation=activ_func_dense, name="fc1")
         self.dropout1 = keras.layers.Dropout(droprate)
         self.fc2 = keras.layers.Dense(4096, activation=activ_func_dense, name="fc2")
         self.dropout2 = keras.layers.Dropout(droprate)
@@ -46,19 +52,19 @@ class AlexNet(keras.Model):
         
     def call(self, inputs):
         X = self.input_Conv2D(inputs)
-        X = keras.layers.BatchNormalization(X)
-     	X = self.MaxPool2D1(X)
+        X = self.batchnorm1(X)
+        X = self.MaxPool2D1(X)
         X = self.Conv2D1(X)
-        X = keras.layers.BatchNormalization(X)
+        X = self.batchnorm2(X)
         X = self.MaxPool2D2(X)
         X = self.Conv2D2(X)
-        X = keras.layers.BatchNormalization(X)
+        X = self.batchnorm3(X)
         X = self.Conv2D3(X)
-        X = keras.layers.BatchNormalization(X)
+        X = self.batchnorm4(X)
         X = self.Conv2D4(X)
-        X = keras.layers.BatchNormalization(X)
+        X = self.batchnorm5(X)
         X = self.MaxPool2D3(X)
-        X = keras.layers.Flatten(X)
+        X = self.flatten_layer(X)
         X = self.fc1(X)
         X = self.dropout1(X)
         X = self.fc2(X)
--- VGG16.py
+++ VGG16.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from tensorflow import keras
-
+tf.config.run_functions_eagerly(True)
 #model = Sequential([
 #    Conv2D(64, (3, 3), input_shape=input_shape, padding='same', activation='relu'),
 #    Conv2D(64, (3, 3), activation='relu', padding='same'),
@@ -27,7 +27,7 @@ from tensorflow import keras
 #+])
 
 class VGG16(keras.Model):
-	def __init__(self, input_shape = (227, 227, 3), activ_func_Conv2D = 'relu', activ_func_dense = 'relu', activ_func_output = 'softmax', num_classes = 8 **kwargs):
+    def __init__(self, input_shape = (227, 227, 3), activ_func_Conv2D = 'relu', activ_func_dense = 'relu', activ_func_output = 'softmax', num_classes = 8, **kwargs):
         super(VGG16, self).__init__(**kwargs)
         # Block 1
         self.input_Conv2D = keras.layers.Conv2D(filters=64, kernel_size=(3,3), activation=activ_func_Conv2D, input_shape=input_shape,  padding="same", name="Block1_Conv1")
@@ -36,7 +36,7 @@ class VGG16(keras.Model):
         # Block 2
         self.Conv2D2 = keras.layers.Conv2D(filters=128, kernel_size=(3,3), activation=activ_func_Conv2D, padding="same", name="Block2_Conv1")
         self.Conv2D3 = keras.layers.Conv2D(filters=128, kernel_size=(3,3), activation=activ_func_Conv2D, padding="same", name="Block2_Conv2")
-		self.MaxPool2D2 = keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name="Block2_pool")
+        self.MaxPool2D2 = keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name="Block2_pool")
         # Block 3
         self.Conv2D4 = keras.layers.Conv2D(filters=256, kernel_size=(3,3), activation=activ_func_Conv2D, padding="same", name="Block3_Conv1")
         self.Conv2D5 = keras.layers.Conv2D(filters=256, kernel_size=(3,3), activation=activ_func_Conv2D, padding="same", name="Block3_Conv2")
@@ -53,6 +53,7 @@ class VGG16(keras.Model):
         self.Conv2D12 = keras.layers.Conv2D(filters=512, kernel_size=(3,3), activation=activ_func_Conv2D, padding="same", name="Block5_Conv3")
         self.MaxPool2D5 = keras.layers.MaxPool2D(pool_size=(2,2), strides=(2,2), name="Block5_pool")
         # Classification block
+        self.flatten_layer = keras.layers.Flatten()
         self.fc1 = keras.layers.Dense(4096, activation=activ_func_dense, name="fc1")
         self.fc2 = keras.layers.Dense(4096, activation=activ_func_dense, name="fc2")
         self.output_layer = keras.layers.Dense(num_classes, activation=activ_func_output, name="prediction")
@@ -60,7 +61,7 @@ class VGG16(keras.Model):
     def call(self, inputs):
         X = self.input_Conv2D(inputs)
         X = self.Conv2D1(X)
-     	X = self.MaxPool2D1(X)
+        X = self.MaxPool2D1(X)
         X = self.Conv2D2(X)
         X = self.Conv2D3(X)
         X = self.MaxPool2D2(X)
@@ -76,7 +77,7 @@ class VGG16(keras.Model):
         X = self.Conv2D11(X)
         X = self.Conv2D12(X)
         X = self.MaxPool2D5(X)
-        X = keras.layers.Flatten(X)
+        X = self.flatten_layer(X)
         X = self.fc1(X)
         X = self.fc2(X)
         return self.output_layer(X)
